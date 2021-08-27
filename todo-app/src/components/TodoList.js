ccd5e5e3c664318047de954663630541f7999ac7
E.Park <epark.grace@gmail.com>
2021-08-27 22:51:12 +0900

    11장까지 예제 학습 완료

---
+++ todo-app/src/components/TodoList.js
@@ -0,0 +1,34 @@
+import React, { useCallback } from 'react';
+import { List } from 'react-virtualized';
+import '../styles/TodoList.scss';
+import TodoListItem from './TodoListItem';
+
+const TodoList = ({ todos, onRemove, onToggle }) => {
+    const rowRenderer = useCallback(
+        ({ index, key, style }) => {
+            const todo = todos[index];
+            return (
+                <TodoListItem todo={todo}
+                              key={key}
+                              onRemove={onRemove}
+                              onToggle={onToggle}
+                              style={style}/>
+            );
+        },
+        [todos, onRemove, onToggle],
+    );
+
+    return (
+        <List className="TodoList"
+              width={512}
+              height={513}
+              rowCount={todos.length}
+              rowHeight={57}
+              rowRenderer={rowRenderer}
+              list={todos}
+              style={{ outline: 'none' }}
+        />
+    );
+};
+
+export default React.memo(TodoList);
